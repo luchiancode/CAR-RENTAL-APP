2ad45d9cacd455e8696a3eb948d6367a881b67ac
DESKTOP-2RGFJTE\Luchian <ionica.luchian@hotmail.com>
2021-04-09 10:27:58 +0300

    no message

---
+++ CAR RENTAL APPLICATION/CAR RENTAL APPLICATION/Areas/Identity/Data/APPLICATIONUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace CAR_RENTAL_APPLICATION.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the APPLICATIONUser class
+    public class APPLICATIONUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string firstName { get; set; }
+
+        [PersonalData]
+        [Column(TypeName = "nvarchar(100)")]
+        public string lastName { get; set; }
+
+
+
+    }
+}
---
+++ CAR RENTAL APPLICATION/CAR RENTAL APPLICATION/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CAR_RENTAL_APPLICATION.Areas.Identity.Data;
+using CAR_RENTAL_APPLICATION.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CAR_RENTAL_APPLICATION.Areas.Identity.IdentityHostingStartup))]
+namespace CAR_RENTAL_APPLICATION.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDbContextConnection")));
+
+                services.AddDefaultIdentity<APPLICATIONUser>(options => { 
+                    
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequiredUniqueChars = 0;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                   
+                })
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
